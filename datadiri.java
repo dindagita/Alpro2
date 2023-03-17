602c8269cd20668eebc97d7714666db14e644d4f
Dindagita <dindagitaa03@gmail.com>
2023-03-17 23:33:21 +0700

    tugas ketiga

---
+++ datadiri.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+   
+class BiodataMahasiswa {
+  public static void main(String args[]){
+            
+    System.out.println("##  Program Java Biodata Mahasiswa ##");
+    System.out.println("========================================");
+    System.out.println();
+    System.out.println("# Data Mahasiswa #");
+    System.out.println("==================");
+    System.out.println("Nama: Dinda Gita Cahyany");
+    System.out.println("NIM: 2022320011");
+    System.out.println("Kelas: FI22B");
+    System.out.println("Jurusan: Sistem Informasi" );
+    System.out.println("Kota Asal: Bekasi");
+    System.out.println("Alamat: Jl. Komplek Surya Mandala no.15");
+  }
+}
